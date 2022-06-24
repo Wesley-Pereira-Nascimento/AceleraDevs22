5514cb82e6388310d54821decfe655b6ba410086
Wesley-Pereira-Nascimento <ylnask@gmail.com>
2022-06-24 14:26:09 -0300

    Atualizando

--- aprendendo-com-eclipse/src/br/com/stefanini/aprendendocomeclipse/operadores/OperadoresAtribuicao.java
+++ aprendendo-com-eclipse/src/br/com/stefanini/aprendendocomeclipse/operadores/OperadoresAtribuicao.java
@@ -3,8 +3,17 @@ package br.com.stefanini.aprendendocomeclipse.operadores;
 public class OperadoresAtribuicao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a = 10; //operador de atribuicao
+		int b = 5;
+		
+		b += a; //aditivo
+		b -= a; //subtrativo
+		b *= a; // multiplicativo
+		b /= a; // divisor
+		b %= a; //modular
+		
+		System.out.println( a % b);
+ 
 	}
 
 }
